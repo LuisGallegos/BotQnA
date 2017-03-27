c2b4816f42debb1715c6aba160f1f3d329f988d5
Luis Gallegos <lags960929@gmail.com>
2017-03-27 16:40:59 -0600

    This is the initial Commit

---
+++ BotFramework/Controllers/BotFrameworkFaqDialog.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using QnAMakerDialog;
+using System;
+using System.Threading.Tasks;
+
+namespace BotFramework
+{
+    [Serializable]
+    [QnAMakerService("810fbe2afab14f299a0b6874ff217cea", "d1cf5bc3-f072-496e-bac3-da49afd29ef5")]
+    public class BotFrameworkFaqDialog : QnAMakerDialog<object>
+    {
+    }
+}
--- BotFramework/Controllers/MessagesController.cs
+++ BotFramework/Controllers/MessagesController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
+using QnAMakerDialog;
 
 namespace BotFramework
 {
@@ -19,51 +21,48 @@ namespace BotFramework
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message)
+            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                //ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
                 // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+                //int length = (activity.Text ?? string.Empty).Length;
 
                 // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                //Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                //await connector.Conversations.ReplyToActivityAsync(reply);
+                await Conversation.SendAsync(activity, () => new BotFrameworkFaqDialog());
             }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
         }
-
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
+        //
+        //private Activity HandleSystemMessage(Activity message)
+        //{
+        //    if (message.Type == ActivityTypes.DeleteUserData)
+         //   {
                 // Implement user deletion here
                 // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
+       //     }
+        //    else if (message.Type == ActivityTypes.ConversationUpdate)
+       //     {
                 // Handle conversation state changes, like members being added and removed
                 // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
                 // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
+       //     }
+        //    else if (message.Type == ActivityTypes.ContactRelationUpdate)
+        //    {
                 // Handle add/remove from contact lists
                 // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
+       //     }
+      //      else if (message.Type == ActivityTypes.Typing)
+       //     {
                 // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
-
-            return null;
-        }
+       //     }
+          //  else if (message.Type == ActivityTypes.Ping)
+       //     {
+        //        Console.WriteLine("ADASD");
+       //     }
+       //
+        //    return null;
+        //}
     }
 }
